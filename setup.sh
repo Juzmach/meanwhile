52ea8d7debc1e8265544a290bb6a63f5c15c453a
Teemu Sarapisto <tsarpf@gmail.com>
2015-02-20 14:21:16 -0500

    Change name

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ cd ../
 
 docker pull dockerfile/nodejs
 docker build -t meanwhile/web:latest .
-docker stop webapp
-docker rm -f webapp
-docker run -p 3000:3000 --name webapp -v $(pwd)/environment:/home/nonroot/environment/src --link db:db_1 -t -i meanwhile/web 
+docker stop mwapp 
+docker rm -f mwapp 
+docker run -p 3000:3000 --name mwapp -v $(pwd)/environment:/home/nonroot/environment/src --link db:db_1 -t -i meanwhile/web 
 #run the following to redirect the traffic coming to your port 80 to the port 3000 that tsatter owns
 #iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000 
