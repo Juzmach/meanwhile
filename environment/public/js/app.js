0cdd6d66420252adec3889c1c296188c6958837d
Tsarpf <tsarpf@gmail.com>
2015-02-20 22:37:37 +0200

    Moved stuff

--- environment/public/js/app.js
+++ environment/public/js/app.js
@@ -1,11 +1,4 @@
 var app = angular.module('meanwhile', ['ngAnimate', 'ui.bootstrap', 'akoenig.deckgrid']);
 
-app.directive('discovery', function() {
-    return {
-        restrict: "E",
-        templateUrl: 'partials/discovery',
-        link: function(scope, element, attrs) {
 
-        }
-    };
-});
+
