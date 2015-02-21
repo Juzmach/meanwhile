602ce4160f4935a38f29cd69b58ea81a27e0e219
Juuso Strömberg <juuso.stromberg@gmail.com>
2015-02-21 12:07:14 +0200

    added controller to index. Added prev and next links

--- environment/public/js/controllers/CardDetailController.js
+++ environment/public/js/controllers/CardDetailController.js
@@ -1,4 +1,6 @@
 angular.module('meanwhileControllers').controller("CardDetailController", ['$scope','$routeParams',
     function($scope,$routeParams){
-    $scope.cardId = $routeParams.cardId;
+        $scope.cardId = $routeParams.cardId;
+        $scope.nextCard = parseInt($scope.cardId) + 1;
+        $scope.prevCard = parseInt($scope.cardId) - 1;
 }]);
