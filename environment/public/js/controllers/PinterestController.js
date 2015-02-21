1d361ee8361ec72bf6e6aedba798ac35481defc2
Tsarpf <tsarpf@gmail.com>
2015-02-21 11:10:06 +0200

    Mostly done

--- environment/public/js/controllers/PinterestController.js
+++ environment/public/js/controllers/PinterestController.js
@@ -3,10 +3,24 @@
  */
 
 angular.module('meanwhileControllers').controller("PinterestController", ['$scope', '$http', function($scope, $http) {
+
     $scope.results = [];
     $scope.loaded = false;
+
+    var from = 0;
+    var increment = 10;
+    var to = from + increment;
+
     $scope.getContent = function() {
-        $http.get('/pinterest/').
+	console.log('oh hi');
+	var reqObj = {
+		url: '/pinterest/',
+		params: {from: from, to: to} 
+	}
+	from += increment;
+	to += increment;
+
+        $http.get(reqObj).
             success(function(data, status, headers, config) {
                 $scope.results = data;
 		console.log(data);
